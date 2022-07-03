3f8dfd86166f96d1675b5a41b729637dd94df0e5
SAHIL0455 <sahilpawar1004@gmail.com>
2022-07-03 17:00:09 +0530

    ADDED: Given massages , ability to analyse and respond happy or sad mood

--- src/test/java/com/bridgelabz/MoodAnalyzerTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyzerTest.java
@@ -1,5 +1,18 @@
 package com.bridgelabz;
 
-public class MoodAnalyzerTest {
+import org.junit.Assert;
+import org.junit.Test;
 
+public class MoodAnalyzerTest {
+    static MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+     @Test
+    public void givenMessageSadThanMoodAnalyzerSad(){
+         String actualMood1 = moodAnalyzer.analyseMood("I am in Sad Mood");
+         Assert.assertEquals("sad",actualMood1);
+     }
+     @Test
+     public void  givenMessageAnyMoodThenMoodAnalyzerHappy(){
+         String actualMood2 = moodAnalyzer.analyseMood("I am in Any Mood");
+         Assert.assertEquals("Happy",actualMood2);
+     }
 }
